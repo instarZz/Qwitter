7e21f7adcb5b67bcfc6fdeb843845f8090938e21
Jerome <instarz@hotmail.fr>
2021-06-04 15:57:10 +0200

    like added

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -7,6 +7,7 @@ use App\Entity\Post;
 use App\Form\CommentFormType;
 use App\Form\PostType;
 use App\Repository\PostRepository;
+use Knp\Component\Pager\PaginatorInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -25,10 +26,12 @@ class PostController extends AbstractController
     }
 
     #[Route('/', name: 'post_index', methods: ['GET'])]
-    public function index(PostRepository $postRepository): Response
+    public function index(PostRepository $postRepository, PaginatorInterface $paginator): Response
     {
+        $posts = $paginator->paginate($postRepository);
+
         return $this->render('post/index.html.twig', [
-            'posts' => $postRepository->findAll(),
+            'posts' => $posts,
         ]);
     }
 
